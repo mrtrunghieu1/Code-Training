3ceb45931ab9da802131bb5b80ac3e74c399a9cf
mrtrunghieu1 <trunghieu02111998@gmail.com>
2020-02-13 17:36:00 +0700

    [ADD] Trainable Combining

--- constant/parameter.py
+++ constant/parameter.py
@@ -1,7 +1,12 @@
 from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import KFold
+
 # Loading model 
 MODEL = [LogisticRegression(random_state=0),
         GaussianNB(),
         KNeighborsClassifier(n_neighbors=10,p = 2)]
+
+#K-Fold
+KF = KFold(n_splits=10)
